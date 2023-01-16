5340dc4f30281888303966dba70190ac8fc23948
miguelfal <falconmiguel.2004@gmail.com>
2023-01-16 22:57:40 +0100

    commit

---
+++ 9.2_POO_en_Java/Ejercicio02/Bicleta.java
@@ -0,0 +1,13 @@
+public class Bicleta extends Vehiculo {
+
+    private int pinones;
+
+    public Bicleta(int p){
+        this.pinones=p;
+    }
+
+    public void hazCaballito(){
+        System.out.println("Hago el caballito");
+    }
+    
+}
