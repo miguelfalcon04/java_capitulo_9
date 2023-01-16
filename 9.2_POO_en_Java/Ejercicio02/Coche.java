e1b7eb80f68fef33cc7d78904b9165f6dd8184be
miguelfal <falconmiguel.2004@gmail.com>
2023-01-16 22:57:53 +0100

    commit

---
+++ 9.2_POO_en_Java/Ejercicio02/Coche.java
@@ -0,0 +1,14 @@
+public class Coche extends Vehiculo {
+
+    private int cilindrada;
+
+    public Coche(int c){
+        super();
+        this.cilindrada=c;
+    }
+
+    public void quemaRueda(){
+        System.out.println("Fffshh");
+    }
+    
+}
