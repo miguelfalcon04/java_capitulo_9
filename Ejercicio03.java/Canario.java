7439ff34025677bbf95debb22bbf9bfc23181586
miguelfal <falconmiguel.2004@gmail.com>
2023-01-17 23:47:25 +0100

    commit

---
+++ Ejercicio03.java/Canario.java
@@ -0,0 +1,20 @@
+public class Canario extends Ave {
+
+    public Canario(String sexo, String nombre){
+        super(sexo,nombre);
+    }
+
+    public void disparaCohetes(){
+        System.out.println("Soy un canario que dispara cohetes");
+    }
+
+    public void pia(){
+        System.out.println("Pio Pio");
+    }
+
+    public void aletea(){
+        System.out.println("Mira como aleteo");
+    }
+
+
+}
