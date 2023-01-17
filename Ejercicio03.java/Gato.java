dc12e218258e3f710eb3e06507f0a36db6bf3921
miguelfal <falconmiguel.2004@gmail.com>
2023-01-17 23:47:36 +0100

    commit

---
+++ Ejercicio03.java/Gato.java
@@ -0,0 +1,21 @@
+public class Gato extends Mamifero {
+
+    public Gato(String sexo, String nombre){
+        super(sexo,nombre);
+    }
+
+    public void salto(){
+        System.out.println("Saltaré al otro tejado");
+        
+    }
+
+    public void mePongoLasBotas(){
+        System.out.println("Soy el gato con botas");
+        
+    }
+
+    public void bebeLeche(){
+        System.out.println("Tenía sed");
+    }
+    
+}
